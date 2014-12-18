8b9e0fd316b5065e7c99b7b1d9863b6380d07fcc
Sahibjot Saggu <sahibjot@live.ca>
2014-12-18 13:05:44 -0500

    Added loading animation

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
+	// Loading Animation
+	$("#loading_bar").animate({'width':'100%'}, 1500);
+	$("#loading_bar").animate({'height':'100%', 'top':'0%'});
+	$("#loading_bar").fadeOut();
+	$("aside").fadeIn();
+	$("aside").delay(1500).animate({'left':'0%'});
+	$("main").delay(2000).animate({'right':'0%'});
 
 });
